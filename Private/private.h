74206ceb41a9d224aefaac4d0178f65d01650ea1
root <root@Leo>
2022-12-29 11:35:40 +0100

    Private class on progress.

--- Private/private.h
+++ Private/private.h
@@ -1,6 +1,33 @@
 #ifndef PRIVATE_H_INCLUDED
 #define PRIVATE_H_INCLUDED
 
+#include "../contact/contact.h"
+
+using namespace std;
+
+struct ddn {
+    int day;
+    int month;
+    int year;
+
+    ddn(int day, int month, int year){
+        this->day=day;
+        this->month=month;
+        this->year=year;
+    }
+};
+
+class Private : public Contact
+{
+    public:
+        virtual ~Private();
+
+        string Getddn();
+        void Setddn(int day, int month, int year);
+
+    private:
+        ddn dateNaissance;
+};
 
 
 #endif // PRIVATE_H_INCLUDED
