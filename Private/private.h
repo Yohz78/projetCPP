8d87f03be7e89d4a274e15dbe7f9e96008b85d36
root <root@Leo>
2022-12-29 10:28:23 +0100

    Projet architecture set up for further work.

---
+++ Private/private.h
@@ -0,0 +1,6 @@
+#ifndef PRIVATE_H_INCLUDED
+#define PRIVATE_H_INCLUDED
+
+
+
+#endif // PRIVATE_H_INCLUDED
---
+++ Pro/pro.h
@@ -0,0 +1,6 @@
+#ifndef PRO_H_INCLUDED
+#define PRO_H_INCLUDED
+
+
+
+#endif // PRO_H_INCLUDED
