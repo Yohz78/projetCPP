a9d7acec89050b36e0df3ee2f41ca771d2b2cf7e
starshooter <starshooter64@yahoo.fr>
2022-12-30 12:31:01 +0100

    work base for menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,26 @@
+
+
+// FAIRE DES FONCTIONS POUR MANIPULER LA BDD
+
+// PENSER AU TYPE DE RETOUR
+// LISTE DES FONCTIONS
+// MANIPULATIONS DES REQUETES
+
+
+void liste(string requete){ // PENSEZ A VERIFIER QUE SI LE CHAMP EST NULL ON NE L AFFICHE PAS
+
+    sqlite3_prepare_v2(db,requete,-1,&stmt, NULL);
+
+    while ( (rc = sqlite3_step(stmt)) == SQLITE_ROW){
+
+    printf("ID: %d", sqlite3_column_int(stmt,0));
+    printf("\nNom: %s %s", sqlite3_column_text(stmt,1), sqlite3_column_text(stmt,2));
+    printf("\nSexe: %s", sqlite3_column_text(stmt,3));
+    printf("\nEntreprise: %s", sqlite3_column_text(stmt,4));
+    printf("\nRue: %d %s", sqlite3_column_int(stmt,5), sqlite3_column_text(stmt,6));
+    printf("\nCP: %d ville: %s", sqlite3_column_int(stmt,7), sqlite3_column_text(stmt,8));
+    printf("\nDate de naissance %d", sqlite3_column_int(stmt,9));
+    printf("\n--------------------------------------\n");
+    }
+    sqlite3_finalize(stmt);
+}
